fb9f90dde46a9a09373c6458b8aec9cafc8552fd
hugoforsberg <fohu23pd@student.ju.se>
2024-11-14 14:16:18 +0100

    Första commit
    Startat projekt och kopplat ihop firebase med projektet

---
+++ app/src/main/java/com/hugo/tictactoe/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.hugo.tictactoe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.hugo.tictactoe.ui.theme.TicTacToeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TicTacToeTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(){
+    val db = Firebase.firestore
+
+    }
